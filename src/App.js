90fd9bea936ce26adf5972272b9826f81eae937e
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-01 09:15:41 +0100

    added a nav bar and a body

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/header/header';
 import MainContent from './components/main-content/main-content';
-
+import Footer from './components/footer/footer';
 function App() {
   return (
     <div className="App">
       <Header />
       <MainContent/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,10 @@
+import './footer.css'
+export default function Footer(){
+    return(
+        
+        <footer>
+            <small>&copy;2024 Daniel All right Reserved </small>
+        </footer>
+        
+    )
+}
