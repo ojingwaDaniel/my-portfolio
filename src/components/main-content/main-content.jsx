3eef888a2641fab24cd35b501cf854129f1e327a
ojingwaDaniel <ojingwadanny@gmail.com>
2024-01-02 04:25:28 +0100

    added my-skills component

--- src/components/main-content/main-content.jsx
+++ src/components/main-content/main-content.jsx
@@ -1,11 +1,13 @@
 import './main-content.css'
 import AboutMe from '../AboutMe/AboutMe';
 import Intro from '../intro/intro';
+import MySkills from '../my-skills/my-skills';
 export default function MainContent(){
     return (
       <div className='main-content'>
         <Intro/>
         <AboutMe/>
+        <MySkills/>
       </div>
     );
 }
---
+++ src/components/my-skills/my-skills.jsx
@@ -0,0 +1,24 @@
+import './my-skills.css'
+export default function MySkills(){
+    return (
+      <div className="my-skills">
+        <div className="box">
+          <img src="" alt="" />
+          <h3>HTML</h3>
+        </div>
+        <div className="box">
+          <img src="" alt="" />
+          <h3>CSS</h3>
+        </div>
+        <div className="box">
+          <img src="" alt="" />
+          <h3>REACT JS</h3>
+        </div>
+        <div className="box">
+          <img src="" alt="" />
+          <h3>JAVASCRIPT</h3>
+        </div>
+      </div>
+    );
+
+}
